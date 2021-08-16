4385dbc45bebf75cfceef56e3ce3a1f8e7e86621
hagiwara <naoki_hagiwara@entrend.net>
2021-08-16 12:01:42 +0900

    管理ユーザーテーブルと初期設定の変更

---
+++ database/migrations/2021_08_14_053818_create_m_admin_user.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * 管理ユーザーテーブルの作成
+ */
+class CreateMAdminUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mAdminUser', function (Blueprint $table) {
+
+            $table->string('userId', 20);
+            $table->string('userName', 20);
+            $table->string('password', 20);
+            $table->string('mail', 255)->nullable();
+            $table->boolean('lockFlg')->default(false);
+            $table->boolean('delFlg')->default(false);
+            $table->dateTime('createDatetime')->nullable();
+            $table->dateTime('updateDatetime')->nullable();
+
+            $table->index(['userId']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mAdminUser');
+    }
+}
