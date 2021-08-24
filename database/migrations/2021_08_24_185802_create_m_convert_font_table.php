2a93b2409105b37372377efb22a4eca45e7a6549
hagiwara <naoki_hagiwara@entrend.net>
2021-08-24 19:05:26 +0900

    旧字体変換マスタ

---
+++ database/migrations/2021_08_24_185802_create_m_convert_font_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * 旧字体変換マスタ
+ */
+class CreateMConvertFontTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mConvertFont', function (Blueprint $table) {
+            $table->string('targetCharacter', 1);
+
+            $table->index(['targetCharacter']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mConvertFont');
+    }
+}
