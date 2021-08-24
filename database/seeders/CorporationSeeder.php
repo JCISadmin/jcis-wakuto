40047725d3fba3f9e71b44468206e2549537b024
hagiwara <naoki_hagiwara@entrend.net>
2021-08-24 20:11:24 +0900

    個人情報、法人情報のseeder

---
+++ database/seeders/CorporationSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * 法人情報
+ */
+class CorporationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        /*
+        DB::table('mCorporation')->insert([
+            'targetCharacter' => '高',
+        ]);
+        */
+
+    }
+
+}
---
+++ database/seeders/PersonSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * 個人情報
+ */
+class PersonSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        /*
+        DB::table('mPerson')->insert([
+            'targetCharacter' => '高',
+        ]);
+        */
+
+    }
+}
