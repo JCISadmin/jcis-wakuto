c8dec0f423a01e9c6421fbff46c63abc013248eb
hagiwara <naoki_hagiwara@entrend.net>
2021-09-02 14:37:46 +0900

    2要素認証テーブルを追加

---
+++ database/migrations/2021_09_02_142008_create_t2_fact_mng_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateT2FactMngTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t2FactMng', function (Blueprint $table) {
+            $table->string('companyId', 20);
+            $table->string('contractPlanId', 20);
+            $table->string('userId', 20);
+            $table->string('manageId', 20);
+            $table->string('ipAddress', 20);
+            $table->string('userAgent', 20);
+            $table->dateTime('createDatetime')->nullable();
+            $table->dateTime('updateDatetime')->nullable();
+
+            $table->index(['companyId', 'contractPlanId', 'userId', 'manageId']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t2FactMng');
+    }
+}
