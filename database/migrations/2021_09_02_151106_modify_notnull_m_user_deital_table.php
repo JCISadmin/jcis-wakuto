97079e253307877c4d2178a06f19c7d313806d28
hagiwara <naoki_hagiwara@entrend.net>
2021-09-02 15:17:54 +0900

    ユーザー詳細テーブルの修正

---
+++ database/migrations/2021_09_02_151106_modify_notnull_m_user_deital_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * mUserDetailのlogoutDatetimeをNullOKへ
+ */
+class ModifyNotnullMUserDeitalTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mUserDetail', function (Blueprint $table) {
+            $table->dateTime('logoutDatetime')->nullable()->change();
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mUserDetail', function (Blueprint $table) {
+            $table->dateTime('logoutDatetime')->nullable(false)->change();
+
+        });
+    }
+}
