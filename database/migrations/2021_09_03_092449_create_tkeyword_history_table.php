24f748b5da34796d399b94635372189678e62d8f
hagiwara <naoki_hagiwara@entrend.net>
2021-09-03 09:32:10 +0900

    検索キーワード履歴

---
+++ database/migrations/2021_09_03_092449_create_tkeyword_history_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * 検索キーワード履歴
+ */
+class CreateTKeywordHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tKeywordHistory', function (Blueprint $table) {
+            $table->string('companyId', 20);
+            $table->string('contractPlanId', 20);
+            $table->string('userId', 20);
+            $table->string('hash', 255);
+            $table->text('keyword');
+            $table->dateTime('searchDate');
+
+            $table->index(['companyId', 'contractPlanId', 'userId', 'hash']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tKeywordHistory');
+    }
+}
