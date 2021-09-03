efd2eefa1c1fbcdb29edce6f59bc287a3e8d9502
三上真由 <mayu_mikami@entrend.net>
2021-09-03 14:26:02 +0900

    ユーザー一覧・詳細・編集のID個数を「有効なID個数」で統一

--- resources/views/manage/user/detail.blade.php
+++ resources/views/manage/user/detail.blade.php
@@ -260,7 +260,7 @@
                                                     <tbody class="bg-white divide-y divide-gray-200">
                                                         <tr>
                                                             <td class="px-5 py-4 whitespace-nowrap text-right text-sm font-medium border">
-                                                                {{ count($userDetailList['contractPlan']['web']['userDetail'])}}
+                                                                {{ $userDetailList['contractPlan']['web']['ids'])}}
                                                             </td>
                                                             <td class="px-5 py-4 whitespace-nowrap text-right text-sm font-medium border">
                                                                 {{ $userDetailList['contractPlan']['web']['idUnitPrice'] }}
