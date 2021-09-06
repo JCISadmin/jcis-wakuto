46fba22d8f50c82d7f6cf84ae0f437463fe5059b
hagiwara <naoki_hagiwara@entrend.net>
2021-09-06 11:52:42 +0900

    バッチ管理テーブルを追加

---
+++ database/migrations/2021_09_06_114253_create_t_mng_batch_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * バッチ管理テーブル
+ */
+class CreateTMngBatchTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tMngBatch', function (Blueprint $table) {
+            $table->string('companyId', 20);
+            $table->string('batchId', 20);
+            $table->text('searchCondition');
+            $table->string('result', 20)->nullable();
+            $table->string('errorCode', 20)->nullable();
+            $table->string('fileName', 80)->nullable();
+            $table->dateTime('createDatetime')->nullable();
+            $table->dateTime('updateDatetime')->nullable();
+
+            $table->index(['companyId', 'batchId']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tMngBatch');
+    }
+}
