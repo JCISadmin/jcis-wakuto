98ddae8ce9b71f114abcbf4bc8f72a36ec90bd25
hagiwara <naoki_hagiwara@entrend.net>
2021-09-06 12:32:59 +0900

    利用明細のリファクタリング

--- app/Http/Controllers/User/UseReportController.php
+++ app/Http/Controllers/User/UseReportController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use App\Models\UseReport;
 
@@ -16,13 +19,16 @@ class UseReportController extends Controller
      * 初期画面表示
      *
      * @param Request $request
+     * @return Application|Factory|View
      */
-    public function index(Request $request)
+    public function index(Request $request): View|Factory|Application
     {
+        $this->actionLog(__CLASS__, __FUNCTION__);
+
         $model = new UseReport();
         $userId = auth()->user()->userId;
         $companyId = auth()->user()->companyId;
-        
+
 
         $assignAry = [
             'useReportList' => $model->getList($companyId, $userId),
