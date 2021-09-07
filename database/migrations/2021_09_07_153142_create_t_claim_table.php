3968188174a1635c5183578ba6fec7f2ffba199f
hagiwara <naoki_hagiwara@entrend.net>
2021-09-07 15:39:30 +0900

    請求テーブルの追加

---
+++ database/migrations/2021_09_07_153142_create_t_claim_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * 請求テーブル
+ */
+class CreateTClaimTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tClaim', function (Blueprint $table) {
+            $table->string('companyId', 20);
+            $table->string('claimMonth', 6);
+            $table->string('claimNo', 20)->nullable();
+            $table->integer('price')->nullable();
+            $table->date('claimDate')->nullable();
+            $table->date('paymentDate')->nullable();
+            $table->string('adjustNote', 20)->nullable();
+            $table->integer('adjustPrice')->nullable();
+            $table->tinyInteger('claimStatus')->nullable();
+            $table->tinyInteger('paymentStatus')->nullable();
+            $table->dateTime('createDatetime')->nullable();
+            $table->dateTime('updateDatetime')->nullable();
+
+            $table->index(['companyId', 'claimMonth']);
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tClaim');
+    }
+}
