82fae6147717419f4be90fe3400f3d4d0526cbbd
三上真由 <mayu_mikami@entrend.net>
2021-09-16 12:30:53 +0900

    dd()の消し忘れを修正

--- app/Http/Controllers/Manage/UserController.php
+++ app/Http/Controllers/Manage/UserController.php
@@ -76,7 +76,6 @@ class UserController extends Controller
             'msg' => $request->session()->get(__CLASS__ . 'msg', ''),
         ];
 
-        dd($userList);
         return view('manage/user/list', $assignAry);
 
     }
