8ada25e59acaf464ff898510f625c3c42a052132
yuushi_ishikawa <yuushi_ishikawa@entrend.net>
2021-09-24 11:45:20 +0900

    zipファイル解凍用パスワード送信メール 文章の変更

--- app/Mail/ZipPasswordInfo.php
+++ app/Mail/ZipPasswordInfo.php
@@ -2,23 +2,28 @@
 
 namespace App\Mail;
 
+use App\Models\MUserCompany;
+use App\Models\MUserDetail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Exception;
 
 class ZipPasswordInfo extends Mailable
 {
     use Queueable, SerializesModels;
 
-    private string $zipPassword;
+    private array $data;
+    private array $company;
+    private array $user;
 
     /**
      * コンストラクタ
      *
      * @param $zipPassword
      */
-    public function __construct($zipPassword) {
-        $this->zipPassword = $zipPassword;
+    public function __construct($data) {
+        $this->data = $data;
     }
 
     /**
@@ -28,10 +33,27 @@ class ZipPasswordInfo extends Mailable
      */
     public function build(): ZipPasswordInfo {
 
+        $companyModel = new MUserCompany();
+        $userModel = new MUserDetail();
+
+        $this->company = $companyModel->get($this->data['companyId']);
+        $this->user = $userModel->get($this->data['companyId'], $this->data['contractPlanId'], $this->data['userId']);
+
+        $mailTitle = '【JCIS反社チェックDBサービス】ID及びパスワードを発行致しました';
+
+        // トライアルの場合、メールタイトルを変更
+        $trialPlan = config('hds.'.'contract')['trialPlan']['web'];
+        if ($this->data['contractPlanId'] === $trialPlan) {
+            $mailTitle = '【JCIS反社チェックDBサービス】トライアルID及びパスワードを発行致しました';
+        }
+
         return $this->text('mail.zipPasswordInfo')
-            ->subject('【JCIS反社チェックDBサービス】zipファイルパスワードを発行致しました')
+            ->subject('※ パスワード通知：'.$mailTitle)
             ->with([
-                'zipPassword' => $this->zipPassword,
+                'companyName' => $this->company['userCompany']['name'],
+                'userName' => $this->user['name'],
+                'zipPassword' => $this->data['zipPassword'],
+                'zipName' => 'JCIS反社DBWEB検索アカウント通知書.zip',
             ]);
     }
 }
