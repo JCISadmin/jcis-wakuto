d1585901a709e24103a6dab8a2f1a73eb56ad5b8
hagiwara <naoki_hagiwara@entrend.net>
2021-09-28 11:52:14 +0900

    利用明細のリファクタ

--- app/Models/UseReport.php
+++ app/Models/UseReport.php
@@ -1,10 +1,12 @@
-<?php
+<?php /** @noinspection PhpArrayShapeAttributeCanBeAddedInspection */
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Facades\DB;
 use Datetime;
+use TCPDF;
 
 /**
  * 利用明細
@@ -26,6 +28,7 @@ class UseReport extends BaseModel
      * @param $companyId
      * @param $userId
      * @return array|null
+     * @throws Exception
      */
     public function getList($companyId, $userId): ?array
     {
@@ -47,6 +50,7 @@ class UseReport extends BaseModel
         $query->where('mUserDetail.companyId', $companyId);
         $query->where('mUserDetail.userId', $userId);
 
+        /** @var object $data */
         $data = $query->first();
 
         $model = new TKeywordHistory();
@@ -56,43 +60,40 @@ class UseReport extends BaseModel
         $year = $today->format('Y');
         $month = $today->format('m');
         $monthSearchCount = $model->getMonthSearchCount($companyId, $userId, $data->contractPlanId, $year, $month);
-        
+
         //年間検索数
-        if(is_null($data->useUpdateDate)){
+        if (is_null($data->useUpdateDate)) {
             //契約更新日が未登録の場合、契約開始日を基準日とする
             $baseDate = new DateTime($data->useStartDate);
-        }else{
+        } else {
             //契約更新日が登録済の場合、契約更新日を基準日とする
             $baseDate = new DateTime($data->useUpdateDate);
         }
 
-        if($today < $baseDate){
+        $dtStart = clone $baseDate;
+        $dtEnd = clone $baseDate;
+        if ($today < $baseDate) {
             //開始日：1年前の基準日
-            $dt = clone $baseDate;
-            $startDate = date_format($dt->modify('-01 year'), 'Y-m-d 0:00:00');
+            $startDate = date_format($dtStart->modify('-01 year'), 'Y-m-d 0:00:00');
             //終了日：基準日の1日前
-            $dt = clone $baseDate;
-            $endDate = date_format($dt->modify('-01 day'), 'Y-m-d 23:59:59');
-        }else{
+            $endDate = date_format($dtEnd->modify('-01 day'), 'Y-m-d 23:59:59');
+        } else {
             //開始日：基準日
-            $dt = clone $baseDate;
-            $startDate = date_format($dt, 'Y-m-d 0:00:00');
+            $startDate = date_format($dtStart, 'Y-m-d 0:00:00');
             //終了日：1年後の基準日の1日前
-            $dt = clone $baseDate;
-            $endDate = date_format($dt->modify('+01 year -01 day'), 'Y-m-d 23:59:59');
+            $endDate = date_format($dtEnd->modify('+01 year -01 day'), 'Y-m-d 23:59:59');
         }
 
         $yearSearchCount = $model->getSearchCount($companyId, $data->contractPlanId, $userId, $startDate, $endDate);
         $depositBalance = $data->deposit - $data->searchUnitPrice * $yearSearchCount;
 
-        $list = [
+        return [
             'monthSearchCount' => $monthSearchCount,
             'yearSearchCount' => $yearSearchCount,
             'depositBalance' => $depositBalance,
 
         ];
 
-        return $list;
     }
 
     /**
@@ -100,9 +101,11 @@ class UseReport extends BaseModel
      *
      * @param $companyId
      * @param $userId
+     * @param $fileName
      * @return string
+     * @throws Exception
      */
-    public function makePdf($companyId, $userId ,$fileName)
+    public function makePdf($companyId, $userId ,$fileName): string
     {
         $dataAry = $this->getList($companyId, $userId);
         $dt = new Datetime();
@@ -112,15 +115,14 @@ class UseReport extends BaseModel
 
         //PDF生成
         $pdfTemplate = 'pdf.pdfUseReport';
-        $pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true,"UTF-8");
+        $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true,"UTF-8");
         $pdf->SetFont('kozminproregular','',9);
         $pdf->setPrintHeader(false);
         $pdf->SetTopMargin(5);
         $pdf->AddPage();
         $pdf->writeHTML(view($pdfTemplate, $dataAry)->render());
 
-        $stream = $pdf->Output( $fileName, "S" );
-        return $stream;
+        return  $pdf->Output( $fileName, "S" );
     }
 
     /**
@@ -128,14 +130,13 @@ class UseReport extends BaseModel
      *
      * @return string
      */
-    public function getFileName()
+    public function getFileName(): string
     {
         $pdfName = '利用明細-%s.pdf';
         $dlDate = date("Ymd");
         $fileName = sprintf($pdfName, $dlDate);
-        $fileName = mb_convert_encoding($fileName, 'SJIS-WIN', 'UTF-8');
+        return mb_convert_encoding($fileName, 'SJIS-WIN', 'UTF-8');
 
-        return $fileName;
     }
 
 }
