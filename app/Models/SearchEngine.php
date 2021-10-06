cc90ed9b0694df38aeace3bd887d034080dca199
三上真由 <mayu_mikami@entrend.net>
2021-10-06 12:07:09 +0900

    検索ワードの大文字変換をupper使用に変更

--- app/Models/SearchEngine.php
+++ app/Models/SearchEngine.php
@@ -150,7 +150,7 @@ class SearchEngine extends BaseModel
         $list = [];
         foreach ($nameList as $item) {
             $query = DB::table('mCorporation');
-            $query->whereRaw('ucase(inputName) = ?', [strtoupper($item)]);
+            $query->whereRaw('ucase(inputName) = ucase(?)', [$item]);
 
             if ($city !== '') {
                 $query->where('address', 'like', $city . '%');
@@ -216,8 +216,8 @@ class SearchEngine extends BaseModel
             $query = DB::table($inQuery);
 
             $query->where(function($query) use($item) {
-                $query->whereRaw('ucase(inputName) = ?', [strtoupper($item)]);
-                $query->orWhereRaw('ucase(inputKana) = ?', [strtoupper($item)]);
+                $query->whereRaw('ucase(inputName) = ucase(?)', [$item]);
+                $query->orWhereRaw('ucase(inputKana) = ucase(?)', [$item]);
 
             });
 
