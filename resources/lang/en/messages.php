0c414d22923e6a0b32ed57294ffbdf5e0887e2bf
三上真由 <mayu_mikami@entrend.net>
2021-10-14 11:27:54 +0900

    請求メールの送信処理中止メッセージを修正

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -10,5 +10,5 @@ return [
     'ERR_NO_SET_EX_RATE' => 'Please registar Exchange Rate. (USD => %s)',
     'INF_NOT_CHECK' => '請求書のメール送信先を選択してください。',
     'INF_SEND_CLAIMMAIL' => '請求対象の会社へメールを送信しました。',
-    'INF_NOT_SEND_CLAIMMAIL' => '請求対象外ため、メール送信を中止しました。',
+    'INF_NOT_SEND_CLAIMMAIL' => '請求対象外のため、メール送信を中止しました。',
 ];
