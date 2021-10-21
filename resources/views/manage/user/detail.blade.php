6e05b1bcfc7470827a851aab2c69769124d689fc
三上真由 <mayu_mikami@entrend.net>
2021-10-21 21:29:37 +0900

    ユーザー詳細を再修正

--- resources/views/manage/user/detail.blade.php
+++ resources/views/manage/user/detail.blade.php
@@ -606,7 +606,7 @@
                     <div class="w-2"></div>
 
                     <button type="submit" onclick="location.href = '{{ route( 'manageUserEdit', ['editId' => $userDetailList['userCompany']['companyId']]) }}';"
-                            class="px-6 py-2 justify-center border border-transparent rounded-md shadow-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400">
+                            class="px-6 py-2 justify-center border border-transparent rounded-md shadow-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400 disabled:opacity-50">
                         編集
                     </button>
                 </div>
