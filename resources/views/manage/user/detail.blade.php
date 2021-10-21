44ce5789eea4f3d96f49dc33dfcc56f5c23acc03
三上真由 <mayu_mikami@entrend.net>
2021-10-21 21:26:44 +0900

    ユーザー詳細の変更を修正

--- resources/views/manage/user/detail.blade.php
+++ resources/views/manage/user/detail.blade.php
@@ -358,7 +358,7 @@
                                                                     {{ $item['mail'] }}
                                                                 </td>
                                                                 <td class="px-2 py-4 text-center whitespace-nowrap text-sm font-medium border">
-                                                                    <button type="button" {{ $userDetailList['userCompany']['contractStatus'] == App\Models\BaseModel::STATUS_TRIAL ? 'disabled' : '' }}
+                                                                    <button type="button" {{ $userDetailList['userCompany']['contractStatus'] == App\Models\BaseModel::STATUS_END ? 'disabled' : '' }}
                                                                             onclick="sendUserInfo('{{ $userDetailList['userCompany']['companyId'] }}', '{{ $userDetailList['contractPlan']['web']['contractPlanId'] }}', '{{ $item['userId'] }}');"
                                                                             class="px-6 py-2 justify-center border border-transparent rounded-md shadow-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400 disabled:opacity-50">
                                                                         通知
