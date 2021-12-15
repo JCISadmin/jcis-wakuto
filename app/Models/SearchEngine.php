ef2c4376e5b1ad4db128dce60d1ae56c5e64e314
yuushi_ishikawa <yuushi_ishikawa@entrend.net>
2021-12-15 16:06:43 +0900

    法人検索、個人検索 同一キーワードで検索結果が複数の場合、事案年月日が直近の順にソート

--- app/Models/SearchEngine.php
+++ app/Models/SearchEngine.php
@@ -157,6 +157,9 @@ class SearchEngine extends BaseModel
                 $query->where('address', 'like', $city . '%');
             }
 
+            $query->orderByRaw('caseDate IS NULL DESC');
+            $query->orderBy('caseDate', 'desc');
+
             $retList = $query->get();
 
             foreach ($retList as $retItem) {
@@ -250,6 +253,9 @@ EOT;
             if ($birthday !== '') {
                 $query->where('birthday', $birthday);
             }
+
+            $query->orderByRaw('caseDate IS NULL DESC');
+            $query->orderBy('caseDate', 'desc');
 
             $retList = $query->get();
 
