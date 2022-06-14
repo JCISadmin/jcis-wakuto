dbf9c253ea1c97488027a6dd9b57b942602f9b22
tatsuya maruyama <80949384+Tatsuya1026@users.noreply.github.com>
2022-06-14 13:45:54 +0900

    カラム追加migration tClaim-memo

---
+++ database/migrations/2022_06_14_123357_modify_t_claim.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyTClaim extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tClaim', function (Blueprint $table) {
+            $table->text('memo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tClaim', function (Blueprint $table) {
+            $table->dropColumn('memo');
+        });
+    }
+}
