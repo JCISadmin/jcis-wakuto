7080c6887c1dc58f715acb38392e06afa4f8b7c3
tatsuya maruyama <80949384+Tatsuya1026@users.noreply.github.com>
2022-06-17 14:18:21 +0900

    kanaカラム追加migration orderBy nullを後回しに修正

--- app/Models/MUserCompany.php
+++ app/Models/MUserCompany.php
@@ -130,6 +130,7 @@ class MUserCompany extends BaseModel
             $query->whereRaw('(webPlanUseEndAlertDate = ? or apiPlanUseEndAlertDate = ?)', [$useEndAlertDate, $useEndAlertDate]);
         }
 
+        $query->orderByRaw('kana IS NULL ASC');
         $query->orderBy('kana','ASC');
 
         if ($pageLine == '') {
