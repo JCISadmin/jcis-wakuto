8ba87d67b8d57ec653624780ad040c8a9df2c16b
tatsuya maruyama <80949384+Tatsuya1026@users.noreply.github.com>
2022-06-17 14:47:36 +0900

    requestファイル　修正

--- app/Http/Requests/Manage/User/UpdateRequest.php
+++ app/Http/Requests/Manage/User/UpdateRequest.php
@@ -17,7 +17,8 @@ class UpdateRequest extends BaseRequest
         return [
             'userCompany.chargeName' => ['nullable','max:20'],
             'userCompany.chargeMail' => ['nullable','email'],
-            'userCompany.companyName' => ['nullable','max:40'],
+            'userCompany.name' => ['nullable','max:40'],
+            'userCompany.kana' => ['nullable','max:40'],
             'userCompany.companyId' => ['required','regex:/^[!-~]+$/','max:5'],
             'userCompany.postCode' => ['nullable', 'digits:7', 'numeric'],
             'userCompany.address' => ['nullable','max:50'],
@@ -79,7 +80,8 @@ class UpdateRequest extends BaseRequest
         return [
             'userCompany.chargeName' => '当社窓口',
             'userCompany.chargeMail' => '当社窓口Email',
-            'userCompany.companyName' => '会社名',
+            'userCompany.name' => '会社名',
+            'userCompany.kana' => '会社名フリガナ',
             'userCompany.companyId' => '会社ID',
             'userCompany.postCode' => '郵便番号',
             'userCompany.address' => '会社住所',
