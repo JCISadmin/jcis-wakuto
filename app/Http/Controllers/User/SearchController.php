a62dcafe90be1992834bbfa91f0d562d2ab3c77c
tatsuya maruyama <80949384+Tatsuya1026@users.noreply.github.com>
2022-06-22 11:19:02 +0900

    roll back

--- app/Http/Controllers/User/SearchController.php
+++ app/Http/Controllers/User/SearchController.php
@@ -17,7 +17,7 @@ use App\Models\TContractPlan;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 /**
- * 即時検索画面
+ * WEB検索画面
  */
 class SearchController extends Controller
 {
@@ -85,7 +85,7 @@ class SearchController extends Controller
     }
 
     /**
-     * 即時検索
+     * WEB検索
      *
      * @param Request $request
      * @return RedirectResponse
