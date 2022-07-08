018ca392bcd1df181705107703b689e4bbc3c8f2
tatsuya maruyama <80949384+Tatsuya1026@users.noreply.github.com>
2022-07-08 19:48:19 +0900

    migrate修正

--- database/migrations/2022_06_29_193513_create_t_contract_plan_detail.php
+++ database/migrations/2022_06_29_193513_create_t_contract_plan_detail.php
@@ -32,10 +32,10 @@ class CreateTContractPlanDetail extends Migration
 
         Schema::table('tContractPlan', function (Blueprint $table) {
             
-            $table->dropColumn('contractTypeId');
-            $table->dropColumn('idUnitPrice');
-            $table->dropColumn('searchUnitPrice');
-            $table->dropColumn('searchCount');
+            $table->integer('contractTypeId')->nullable()->default(null)->change();
+            $table->integer('idUnitPrice')->nullable()->default(0)->change();
+            $table->integer('searchUnitPrice')->nullable()->default(0)->change();
+            $table->integer('searchCount')->nullable()->default(0)->change();
 
         });
 
@@ -52,10 +52,10 @@ class CreateTContractPlanDetail extends Migration
 
         Schema::table('tContractPlan', function (Blueprint $table) {
             
-            $table->string('contractTypeId', 20);
-            $table->integer('idUnitPrice')->nullable();
-            $table->integer('searchUnitPrice')->nullable();
-            $table->integer('searchCount')->nullable();
+            $table->string('contractTypeId', 20)->change();
+            $table->integer('idUnitPrice')->nullable()->change();
+            $table->integer('searchUnitPrice')->nullable()->change();
+            $table->integer('searchCount')->nullable()->change();
 
         });
 
