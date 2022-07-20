078a97e860425c1cf4855775e9fee96d692580b0
tatsuya maruyama <80949384+Tatsuya1026@users.noreply.github.com>
2022-07-20 09:58:47 +0900

    コメント追加

--- app/Models/UsageStatus.php
+++ app/Models/UsageStatus.php
@@ -79,6 +79,7 @@ class UsageStatus extends Report
             'tContractPlanDetail.companyId',
             'tContractPlanDetail.contractPlanId',
             DB::raw('sum(searchCnt.searchCount) as totalCount'),
+            //chargeFlg = 0 かつ 全額デポジット は検索単価0で集計
             DB::raw('group_concat(IF(searchCnt.chargeFlg=0 AND tContractPlanDetail.contractTypeId = "allDepo", 0, tContractPlanDetail.searchUnitPrice)) as unitPriceAry'),
             DB::raw('group_concat(searchCnt.searchCount) as countAry'),
         );
