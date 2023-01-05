fa6d4a9d942ce3464f2c4d3a7c2dc603f7cffe14
tatsuya maruyama <80949384+Tatsuya1026@users.noreply.github.com>
2023-01-05 10:51:27 +0900

    apiテスト用キーを設定

--- config/acuris.php
+++ config/acuris.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'apiKey' => 'xxx',
+    'apiKey' => 'd7ce6522-8efd-4361-a3a3-bc70186e5b05',
     'uri' => [
         'businesses' => 'http://dev.api.acuris.com/compliance/businesses',
         'individuals' => 'http://dev.api.acuris.com/compliance/individuals',
