ec6b65172ff05d7eadfc54b7f5b2fedf95457719
tatsuya maruyama <80949384+Tatsuya1026@users.noreply.github.com>
2023-01-19 14:46:31 +0900

    tKeywordPreviousHistory migration追加

---
+++ database/migrations/2023_01_19_110934_create_t_keyword_previous_history.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTKeywordPreviousHistory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tKeywordPreviousHistory', function (Blueprint $table) {
+            $table->string('companyId', 20);
+            $table->string('contractPlanId', 20);
+            $table->string('userId', 20);
+            $table->string('hash', 255);
+            $table->integer('seqNo');
+            $table->text('keyword');
+            $table->dateTime('searchDate');
+            $table->tinyInteger('chargeFlg')->nullable()->default(0);
+            $table->integer('freeSearchCount');
+
+            $table->primary(['companyId', 'contractPlanId', 'userId', 'hash', 'seqNo'],'PRIMARY_NAME');
+        });
+
+        Schema::table('tKeywordHistory', function (Blueprint $table) {
+            $table->integer('freeSearchCount');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tKeywordPreviousHistory');
+
+        Schema::table('tKeywordHistory', function (Blueprint $table) {
+            $table->dropColumn('freeSearchCount');
+        });
+    }
+}
