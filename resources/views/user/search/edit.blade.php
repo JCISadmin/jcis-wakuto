6be95b8249056f384c4824cb5f05f02435877a32
tatsuya maruyama <80949384+Tatsuya1026@users.noreply.github.com>
2023-01-30 18:13:25 +0900

    検索履歴 追加テーブルではなくexpireDateでの管理に変更

--- resources/views/user/search/edit.blade.php
+++ resources/views/user/search/edit.blade.php
@@ -34,7 +34,7 @@
                         @for ($i = 0; $i < 10; $i++)
                             <div class="border w-full">
                                 <label>
-                                    <input type="text" placeholder="個人名" maxlength="130" value="{{ old('parsonName.'.$i, '') }}" name="parsonName[]"
+                                    <input type="text" placeholder="個人名" maxlength="130" value="{{ old('personName.'.$i, '') }}" name="personName[]"
                                            class="checkPerson w-full px-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500">
                                 </label>
                             </div>
