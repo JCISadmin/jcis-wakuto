ed671efc7e424c670eb03cb3979792ce33b4f1fa
tatsuya maruyama <80949384+Tatsuya1026@users.noreply.github.com>
2023-02-27 10:45:12 +0900

    kernel 実行時間修正

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('UserEndAlert')->dailyAt('09:57');
+        $schedule->command('UserEndAlert')->dailyAt('10:00');
     }
 
     /**
