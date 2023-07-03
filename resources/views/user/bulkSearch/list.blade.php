f4801cbb7ff3a12e1d0e3c7b6ffaafba7919ca9d
hagiwara <naoki_hagiwara@entrend.net>
2023-07-03 10:55:57 +0900

    #12717のPDFプレビュー対応

--- resources/views/user/bulkSearch/list.blade.php
+++ resources/views/user/bulkSearch/list.blade.php
@@ -79,10 +79,18 @@
                                             </td>
                                             <td class="px-3 py-2 whitespace-nowrap text-sm text-center font-medium border">
                                                 @if ($item->result == App\Models\BaseModel::BATCH_DONE && $item->delFlg == 0)
-                                                    <button type="button" onclick="location.href = '{{ route('userBulkSearchResult', ['batchId' => $item->batchId, 'type' => 'pdf']) }}';"
-                                                        class="px-6 py-2 justify-center border border-transparent rounded-md shadow-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400">
-                                                        PDF
-                                                    </button>
+                                                    @if ($title == '登記情報検索一覧画面')
+                                                        <button type="button" onclick="window.open('{{ route('userBulkSearchResult', ['batchId' => $item->batchId, 'type' => 'pdf']) }}', '_blank');"
+                                                            class="px-6 py-2 justify-center border border-transparent rounded-md shadow-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400">
+                                                            PDF
+                                                        </button>
+                                                    @else
+                                                        <button type="button" onclick="location.href = '{{ route('userBulkSearchResult', ['batchId' => $item->batchId, 'type' => 'pdf']) }}';"
+                                                                class="px-6 py-2 justify-center border border-transparent rounded-md shadow-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400">
+                                                            PDF
+                                                        </button>
+                                                    @endif
+
                                                     <button type="button" onclick="location.href = '{{ route('userBulkSearchResult', ['batchId' => $item->batchId, 'type' => 'csv']) }}';"
                                                         class="px-6 py-2 justify-center border border-transparent rounded-md shadow-sm font-medium text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400">
                                                         CSV
