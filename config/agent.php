f594aa38b96a47bc554d7bfe37e520521b7b3464
tatsuya maruyama <80949384+Tatsuya1026@users.noreply.github.com>
2024-08-13 15:12:35 +0900

    代理店利用状況の時のDB接続先制御

---
+++ config/agent.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'agentList'=> [
+        1 => [
+            'name' => '代理店A',
+            'dbConnection' => 'mysql_agent1',
+        ],
+        2 => [
+            'name' => '代理店B',
+            'dbConnection' => 'mysql',
+        ]
+    ]
+];
