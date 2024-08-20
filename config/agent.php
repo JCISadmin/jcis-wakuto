96c3a355ee878442bff8fd9b2216acd086512933
tatsuya maruyama <80949384+Tatsuya1026@users.noreply.github.com>
2024-08-20 18:01:48 +0900

    代理店DB設定修正

--- config/agent.php
+++ config/agent.php
@@ -4,11 +4,7 @@ return [
     'agentList'=> [
         1 => [
             'name' => '代理店A',
-            'dbConnection' => 'mysql_agent1',
-        ],
-        2 => [
-            'name' => '代理店B',
-            'dbConnection' => 'mysql',
+            'dbConnection' => 'mysql_agent1'
         ]
     ]
 ];
