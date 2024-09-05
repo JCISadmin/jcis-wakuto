d84b072dd7867f23d4635f1ad33d3ef19cb3334b
tatsuya maruyama <80949384+Tatsuya1026@users.noreply.github.com>
2024-09-05 12:17:04 +0900

    利用状況修正

--- app/Models/UsageStatus.php
+++ app/Models/UsageStatus.php
@@ -697,7 +697,7 @@ class UsageStatus extends BaseModel
      * 検索情報取得クエリを生成
      * @param $type
      * @param $searchCnt
-     * @return 
+     * @return
      */
     private function buildQuerySearchInfo($type, $searchCnt)
     {
@@ -708,7 +708,7 @@ class UsageStatus extends BaseModel
             DB::raw('sum(searchCnt.searchCount) as totalCount'),
             //chargeFlg = 0 かつ 全額デポジット は検索単価0で集計
             DB::raw('group_concat(IF(searchCnt.chargeFlg=0 AND tContractPlanDetail.contractTypeId = "allDepo", 0, tContractPlanDetail.searchUnitPrice)) as unitPriceAry'),
-            DB::raw('group_concat(searchCnt.searchCount) as countAry'),
+            DB::raw('group_concat(IFNULL(searchCnt.searchCount, 0)) as countAry'),
         );
         $searchInfo->leftJoin('mContractPlan', function ($join) {
             $join->on('tContractPlanDetail.contractPlanId', '=', 'mContractPlan.contractPlanId');
